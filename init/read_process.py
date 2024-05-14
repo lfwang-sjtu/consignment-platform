f20e0844b821cb963ccfd6fd1996d132da0931b6
lfwang-sjtu <wanglefeng@sjtu.edu.cn>
2024-05-14 12:57:53 +0800

    py script for orchestration

---
+++ init/read_process.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+# 定义 URL 地址
+url = 'http://202.120.40.86:14642/rmp-resource-service/project/66289c8cdffd2d00144103a2/resource/Orchestration'
+
+# 发送 GET 请求并获取响应内容
+response = requests.get(url)
+
+response_data = response.json()
+
+for item in response_data['data']:
+    item['process'] = item['process'].split(',')
+
+# 格式化输出响应内容
+print('Orchestration Response:', json.dumps(response_data, indent=4))
---
+++ init/write_process.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+# 读取 JSON 文件
+with open('process.json', 'r') as file:
+    data = json.load(file)
+
+for orchestration in data['Orchestration']:
+    process_str = ','.join(orchestration['process'])
+    orchestration['process'] = process_str
+    url = 'http://202.120.40.86:14642/rmp-resource-service/project/66289c8cdffd2d00144103a2/resource/Orchestration'
+    response = requests.post(url, json=orchestration)
+    print('Orchestration Response:', response.text)
