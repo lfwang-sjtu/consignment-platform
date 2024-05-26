c418f1eec5905541202b74b81cf691d11cde78ec
ljm567 <ljm567@sjtu.edu.cn>
2024-05-26 21:50:49 +0800

    完善了公司信息展示页面

--- web-frontend/src/view/com/CompanyView.js
+++ web-frontend/src/view/com/CompanyView.js
@@ -1,52 +1,60 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Descriptions, Button, Modal, Form, Input, Layout, Menu} from 'antd';
 import {Footer, Header} from "antd/es/layout/layout";
 import FootInfo from "../../component/FootInfo";
 
 function CompanyView() {
     const [modalVisible, setModalVisible] = useState(false);
-    const [formData, setFormData] = useState({
-        company: '天天好',
-        password: '123',
-        username: 'User1',
-        email: '123@qq.com',
-        phone: '123456',
-        address: '东川路800号',
-        intro: '一家公司',
-        status: '1',
-        agreement: 'hash号码',
-        joinData: '2024-01-01',
-        manageFee: 'XXX',
-    });
+    const [data, setData] = useState(JSON.parse(localStorage.getItem('CompanyUser')));
+    const id = data.id;
 
     const onFinish = (values) => {
-        console.log(values); // 打印用户输入的数据
-        setFormData(values);
+        console.log(values);
+        fetch(`http://202.120.40.86:14642/rmp-resource-service/project/66289c8cdffd2d00144103a2/resource/CompanyUser/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values)
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result.data);
+                setData(result.data)
+                localStorage.setItem('CompanyUser', JSON.stringify(data))
+        })
         setModalVisible(false);
     };
 
     return (
-
             <div>
                 <Descriptions title="Company User Info" bordered>
-                    {Object.entries(formData).map(([key, value]) => (
-                        <Descriptions.Item label={key} key={key}>{value}</Descriptions.Item>
-                    ))}
+                    {Object.entries(data).map(([key, value]) => {
+                        if (key !== 'id' && key !== 'status') {
+                            return <Descriptions.Item label={key} key={key}>{value}</Descriptions.Item>
+                        }
+                        return null;
+                    })}
                 </Descriptions>
                 <Button type="primary" onClick={() => setModalVisible(true)}>
                     Update
                 </Button>
                 <Modal title="Update" visible={modalVisible} onCancel={() => setModalVisible(false)} footer={null}>
-                    <Form name="update_form" onFinish={onFinish}>
-                        {Object.keys(formData).map((key) => (
-                            <Form.Item
-                                name={key}
-                                key={key}
-                                rules={[{required: true, message: `Please input the ${key}!`}]}
-                            >
-                                <Input placeholder={key}/>
-                            </Form.Item>
-                        ))}
+                    <Form name="update_form" onFinish={onFinish} initialValues={data}>
+                        {Object.keys(data).map((key) => {
+                            if (key !== 'id' && key !== 'status') {
+                                return (
+                                    <Form.Item
+                                        name={key}
+                                        key={key}
+                                        rules={[{required: true, message: `Please input the ${key}!`}]}
+                                    >
+                                        <Input placeholder={key}/>
+                                    </Form.Item>
+                                )
+                            }
+                            return null;
+                        })}
                         <Form.Item>
                             <Button type="primary" htmlType="update">
                                 Update
